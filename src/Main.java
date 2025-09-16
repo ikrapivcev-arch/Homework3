4a4b35a361a57d797afd68c9b98757ab0eb3b5cb
Илья <ikrapivcev@gmail.com>
2025-09-16 21:00:33 +0300

    Готовое ДЗ. В 7 задани не пойму где ошибка. 8 задание решил с помощью ИИ. не смог сам разобраться.

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,134 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Задание 1 ");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        byte shovels = 2;
+        System.out.println(shovels);
+        short bucket = 31500;
+        System.out.println(bucket);
+        int bag = 1587480;
+        System.out.println(bag);
+        long gloves = 525648974;
+        System.out.println(gloves);
+        float rope = 2.8f;
+        System.out.println(rope);
+        double box = 0;
+        System.out.println(box);
+
+        System.out.println("Задание 2 ");
+
+        float a = 27.12f;
+        System.out.println(a);
+        long b = 987678965549L;
+        System.out.println(b);
+        double c = 2.785f;
+        System.out.println(c);
+        short d = 569;
+        System.out.println(d);
+        short e = -159;
+        System.out.println(e);
+        int g = 27897;
+        System.out.println(g);
+        byte x = 67;
+        System.out.println(x);
+
+        System.out.println(" Задание 3 ");
+
+        byte lStudent = 23;
+        byte aStudent = 27;
+        byte eStudent = 30;
+        byte allStudent = (byte) (lStudent + aStudent + eStudent);
+        System.out.println(allStudent + " Студентов ");
+        int paper = 480;
+        int forEveryStudent = paper / allStudent;
+        System.out.println(" На каждого ученика рассчитано " + forEveryStudent + " листов бумаги ");
+
+        System.out.println(" Задание 4 ");
+
+        byte minutes = 2;
+        short bottles = 16;
+        short bottlesInMinutes = (short) (bottles / minutes);
+        int timeMinutes = 20;
+        int bottlesIn20Minutes = bottlesInMinutes * timeMinutes;
+        System.out.println(" За 20 минут машина произвела " + bottlesIn20Minutes + " штук бутылок ");
+        short month = 60;
+        short day = (short) (month * 24);
+        short bottlersInDay = (short) ( day * bottlesInMinutes);
+        System.out.println(" В сутки машина произвела " + bottlersInDay + " штук бутылок ");
+        int bottlersIn3Day = bottlersInDay * 3;
+        System.out.println(" За три дня машина произвела " + bottlersIn3Day + " штук бутылок");
+        int bottleraInMonth = bottlersInDay * 30;
+        System.out.println(" За 1 месяц машина произвела " + bottleraInMonth + " штук бутылок ");
+
+        System.out.println(" Задание 5 ");
+
+        short totalCansOfPaint = 120;
+        short whitePaint = 2;
+        short brownPaint = 4;
+        int paintPerClass = whitePaint + brownPaint;
+        int numberOfClasses = totalCansOfPaint / paintPerClass;
+        int totalWhitePaint = numberOfClasses * whitePaint;
+        int totalBrownPaint = numberOfClasses * brownPaint;
+        System.out.println(" В школе, где " + numberOfClasses + " классов, нужно " + totalWhitePaint + " банок белой краски и " + totalBrownPaint + " банок коричневой краски ");
+
+
+        System.out.println(" Задание 6 ");
+
+        int weightOfBananas = 5 * 80;
+        int weightOfMilk = 200 * 105;
+        int weightOfIceCream = 2 * 100;
+        int weightEggs = 4 * 70;
+        int totalGrams = weightOfBananas + weightOfMilk + weightOfIceCream + weightEggs;
+        double totalKilograms = totalGrams / 1000.0;
+        System.out.println(" Вес завтрака в граммах " + totalGrams + " грамм ");
+
+        System.out.println(" Задание 7 ");
+
+        short weightToLoseKg = 7;
+        double weightToLoseGrams = weightToLoseKg * 1000.0;
+        float minWeightToLossPerDay = 250.0f;
+        float maxWeightToLossPerDay = 500.0f;
+        int daysAtMinWeightLoss = (int) ( weightToLoseGrams / minWeightToLossPerDay);
+        System.out.println(" При потере 250 грамм в день потребуется " + daysAtMinWeightLoss + " дней ");
+        int daysAtMaxWeightLoss = (int) (weightToLoseGrams / maxWeightToLossPerDay);
+        System.out.println(" При потере 500 грамм в день потребуется " + daysAtMaxWeightLoss + " дней ");
+        float averageWeightLossPerDay = (minWeightToLossPerDay + maxWeightToLossPerDay) / 2f;
+        int averageDays = (int) (weightToLoseGrams / averageWeightLossPerDay);
+        System.out.println(" В среднем потребуется " + averageDays + " дней");
+
+
+        System.out.println(" Задание 8 ");
+
+        float mashaSalary = 67760.0f;
+        float denisSalary = 83690.0f;
+        float kristinaSalary = 76230.0f;
+
+        byte increasePercentage = 10;
+
+        float mashaIncrease = mashaSalary * (increasePercentage / 100.0f);
+        float mashaNewSalary = mashaSalary + mashaIncrease;
+        float mashaAnnualDifference = mashaIncrease * 12;
+        System.out.println("Маша теперь получает " + mashaNewSalary + " рублей. Годовой доход вырос на " + mashaAnnualDifference + " рублей.");
+
+        float denisIncrease = denisSalary * (increasePercentage / 100.0f);
+        float denisNewSalary = denisSalary + denisIncrease;
+        float denisAnnualDifference = denisIncrease * 12;
+        System.out.println("Денис теперь получает " + denisNewSalary + " рублей. Годовой доход вырос на " + denisAnnualDifference + " рублей.");
+
+        float kristinaIncrease = kristinaSalary * (increasePercentage / 100.0f);
+        float kristinaNewSalary = kristinaSalary + kristinaIncrease;
+        float kristinaAnnualDifference = kristinaIncrease * 12;
+        System.out.println("Кристина теперь получает " + kristinaNewSalary + " рублей. Годовой доход вырос на " + kristinaAnnualDifference + " рублей.");
+
+
+
+
+
+
+
+
+
+
         }
     }
-}
